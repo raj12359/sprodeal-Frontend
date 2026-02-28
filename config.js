43bb964a57b337aeb0d4198953d500e35ed4a682
Rajeev Kumar <rajeevkumarsingh913572@gmail.com>
2026-02-28 20:30:16 +0530

    update

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ const API_BASE_URL =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? "http://localhost:5000"   // Development
-    : "https://sprodeal-backend.onrender.com"; // Production
+    : "https://sprodeal-backend-tjf4.onrender.com"; // Production
 
 export { API_BASE_URL };
